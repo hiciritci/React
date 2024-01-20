46c6982c51cd009a15387c21d5483acb67adf73e
hiciritci <halilcrtc@gmail.com>
2024-01-21 01:55:19 +0300

    FirstPush

---
+++ my-project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png"></img>
+      <img src={logo}></img>
+      Halil İbrahim CIRITCI
+    </div>
+  );
+}
+
+export default App;
